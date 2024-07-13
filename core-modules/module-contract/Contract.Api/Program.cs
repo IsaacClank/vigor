e41b4c38c3ebdaa6c31b6c12c39bdfbc78f167ac
isaac <minimaleffort769@gmail.com>
2024-07-13 19:01:19 +0700

    impl: initial commit for Contract module

---
+++ core-modules/module-contract/Contract.Api/Program.cs
@@ -0,0 +1,31 @@
+using Contract.Database;
+
+namespace Contract.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.AddContractDbContext();
+            builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+
+    public static class ProgramExtensions
+    {
+        public static void AddContractDbContext(this IHostApplicationBuilder builder) => builder.Services.AddContractDbContext(
+            builder.Configuration.GetConnectionString("ContractDatabase")
+                ?? throw new Exception("Database connection string cannot be null.")
+        );
+    }
+}
