1b11ad33a095e3608949e3b5c48000e68018ec49
isaac <minimaleffort769@gmail.com>
2024-07-25 13:43:33 +0700

    chore: update .editorconfig

--- core/module-user/User.Database/Migrations/20240721105730_CreateOutboxReplicationSlot.cs
+++ core/module-user/User.Database/Migrations/20240721105730_CreateOutboxReplicationSlot.cs
@@ -4,19 +4,19 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace User.Database.Migrations
 {
+  /// <inheritdoc />
+  public partial class CreateOutboxReplicationSlot : Migration
+  {
     /// <inheritdoc />
-    public partial class CreateOutboxReplicationSlot : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.Sql(@"SELECT * FROM pg_create_logical_replication_slot('outbox', 'wal2json')");
-        }
-
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.Sql(@"SELECT pg_drop_replication_slot('outbox')");
-        }
+      migrationBuilder.Sql(@"SELECT * FROM pg_create_logical_replication_slot('outbox', 'wal2json')");
     }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.Sql(@"SELECT pg_drop_replication_slot('outbox')");
+    }
+  }
 }
