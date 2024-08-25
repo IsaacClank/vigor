c7806a10473a83255ef886b66d9d08d3b93257b3
isaac <minimaleffort769@gmail.com>
2024-08-25 18:12:38 +0700

    chore: common HTTP response utility library
    Common `ApiResponse` that conform somewhat
    to [JSON:API](https://jsonapi.org).

---
+++ core/Vigor.Core.Common.Http.Response/ApiResponse.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Vigor.Core.Common.Http.Response;
+
+public class ApiError
+{
+  [Required]
+  public required string Title { get; set; }
+
+  [Required]
+  public required string Detail { get; set; }
+
+  [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+  public string? StackTrace { get; set; }
+}
+
+public class ApiResponse
+{
+  [Required]
+  public Dictionary<string, object> Meta { get; set; } = [];
+
+  [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+  public IEnumerable<ApiError>? Errors { get; set; }
+
+  public ApiResponse() { }
+  public ApiResponse(params Exception[] exceptions)
+  {
+    Errors = exceptions.Select(e => new ApiError
+    {
+      Title = e.GetType().Name[..^2],
+      Detail = e.Message,
+      StackTrace = e.StackTrace,
+    });
+  }
+}
+
+public class ApiResponse<T>(T data) : ApiResponse
+{
+  [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+  public T? Data { get; set; } = data;
+}
