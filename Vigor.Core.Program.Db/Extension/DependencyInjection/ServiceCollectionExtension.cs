238dd3018cc0c6f0892d22a6ab3a6145872addec
isaac <minimaleffort769@gmail.com>
2024-08-26 22:51:32 +0700

    impl(program,db): add DI config

---
+++ Vigor.Core.Program.Db/Extension/DependencyInjection/ServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
+
+using Vigor.Core.Common.Db.PostgreSql.Extensions.DependencyInjection;
+
+namespace Vigor.Core.Program.Db.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtension
+{
+  public static void AddProgramDbContext(
+    this IServiceCollection services,
+    string? connectionString) => services.AddPostgreSqlDbContext<ProgramDbContext>(connectionString);
+
+  public static void AddProgramDbContextFactory(
+    this IServiceCollection services,
+    string? connectionString) => services.AddPostgreSqlDbContextFactory<ProgramDbContext>(connectionString);
+}
