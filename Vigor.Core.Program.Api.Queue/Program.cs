883ea25e14a1a00f048c4107f8265f9baf13eec8
isaac <minimaleffort769@gmail.com>
2024-08-30 17:03:14 +0700

    impl(program,queue): initial queue consumer

---
+++ Vigor.Core.Program.Api.Queue/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.Extensions.Options;
+
+using Vigor.Core.Common.Queue.Redis.Extensions.DependencyInjection;
+using Vigor.Core.Program.Api.Queue;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+#region Options
+builder.Services.Configure<QueueConsumerOptions>(builder.Configuration.GetSection("QueueConsumer"));
+#endregion
+
+#region Redis clients
+builder.Services.AddRedisClient(builder.Configuration.GetConnectionString("Queue"));
+builder.Services.AddSingletonRedisStreamConsumer(provider =>
+{
+  var options = provider.GetRequiredService<IOptions<QueueConsumerOptions>>();
+
+  var queueConsumerOptions = provider.GetRequiredService<IOptions<QueueConsumerOptions>>().Value;
+  return new()
+  {
+    Stream = queueConsumerOptions.Stream,
+    Group = queueConsumerOptions.Group,
+    Consumer = queueConsumerOptions.Consumer,
+  };
+});
+#endregion
+
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
