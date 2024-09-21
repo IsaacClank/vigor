267b8823f56b764b5e67e47ab9bc1179bbd28fe5
isaac <minimaleffort769@gmail.com>
2024-09-21 16:20:39 +0700

    chore: rename development migration script

--- ef-db-update.sh
+++ ef-db-update.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 WORKDIR=$(cd $(dirname $0); pwd)
-cd "${WORKDIR}/Vigor.Core.Program.Db/" &>/dev/null
 
+cd "${WORKDIR}/Vigor.Core.Program.Db/" &>/dev/null
 env DB_CONNECTION="${PROGRAM_DB}" dotnet ef database update
 
 cd - &>/dev/null
