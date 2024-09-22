3ea6a31047754c7ba4d1518a9ade9f9a32fb8f41
isaac <minimaleffort769@gmail.com>
2024-09-22 22:07:45 +0700

    refactor(core,program,facility): additional loggings

--- Vigor.Core.Program.Domain.Facility/FacilityCrud.cs
+++ Vigor.Core.Program.Domain.Facility/FacilityCrud.cs
@@ -1,35 +1,41 @@
 ﻿using AutoMapper;
 
+using Microsoft.Extensions.Logging;
+
 using Vigor.Common.Db.Repository;
 using Vigor.Common.Exception;
+using Vigor.Common.Extensions.Logging;
+using Vigor.Common.Extensions.System;
 using Vigor.Core.Program.Domain.Facility.Contracts;
 using Vigor.Core.Program.Domain.Facility.Interfaces;
 
 namespace Vigor.Core.Program.Domain.Facility;
 
-public class FacilityCrud(IMapper mapper, IUnitOfWork unitOfWork) : IFacilityCrud
+public class FacilityCrud(
+  ILogger<FacilityCrud> logger,
+  IMapper mapper,
+  IUnitOfWork unitOfWork) : IFacilityCrud
 {
+  private ILogger<FacilityCrud> Logger { get; } = logger;
   private readonly IMapper _mapper = mapper;
   private readonly IUnitOfWork _unitOfWork = unitOfWork;
 
   private IRepository<Db.Entities.Facility> FacilityRepository => _unitOfWork.Repository<Db.Entities.Facility>();
 
-  public async Task<Contracts.Facility> UpsertAsync(Guid userId, UpsertFacility createFacility)
+  public async Task<Contracts.Facility> UpsertAsync(
+    Guid userId,
+    UpsertFacility upsertFacility)
   {
-    var facility = _mapper.Map<Db.Entities.Facility>(createFacility);
-    facility.OwnerId = userId;
+    var facilityToUpsert = _mapper.Map<Db.Entities.Facility>(upsertFacility);
+    facilityToUpsert.OwnerId = userId;
 
-    if (facility.Id != Guid.Empty)
-    {
-      FacilityRepository.Update(facility);
-    }
-    else
-    {
-      FacilityRepository.Insert(facility);
-    }
-
+    var upsertedFacility = upsertFacility.Id.IsEmptyOrDefault()
+      ? await FacilityRepository.InsertAsync(facilityToUpsert)
+      : FacilityRepository.Update(facilityToUpsert);
     await _unitOfWork.SaveAsync();
-    return _mapper.Map<Contracts.Facility>(facility);
+    Logger.UpsertedEntity(nameof(Db.Entities.Facility), upsertedFacility.Id);
+
+    return _mapper.Map<Contracts.Facility>(upsertedFacility);
   }
 
   public async Task<IEnumerable<Contracts.Facility>> FindAsync(Guid userId)
@@ -42,8 +48,11 @@ public class FacilityCrud(IMapper mapper, IUnitOfWork unitOfWork) : IFacilityCru
   {
     var facility = await FacilityRepository.FindAsync(facilityId);
     EntityNotFoundException.ThrowIfNull(facility);
+
     FacilityRepository.Delete(facility);
     await _unitOfWork.SaveAsync();
+    Logger.DeletedEntity(nameof(Db.Entities.Facility), facilityId);
+
     return _mapper.Map<Contracts.Facility>(facility);
   }
 }
--- Vigor.Core.Program.Domain.Facility/Interfaces/IFacilityCrud.cs
+++ Vigor.Core.Program.Domain.Facility/Interfaces/IFacilityCrud.cs
@@ -2,7 +2,7 @@ namespace Vigor.Core.Program.Domain.Facility.Interfaces;
 
 public interface IFacilityCrud
 {
-  public Task<Contracts.Facility> UpsertAsync(Guid userId, Contracts.UpsertFacility createFacility);
+  public Task<Contracts.Facility> UpsertAsync(Guid userId, Contracts.UpsertFacility upsertFacility);
 
   public Task<IEnumerable<Contracts.Facility>> FindAsync(Guid userId);
 
